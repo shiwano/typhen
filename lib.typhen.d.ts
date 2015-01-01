2b2975d1a73f440919f0720cf36665fa3bccd146
Shogo Iwano <shiwano@gmail.com>
2015-01-02 04:41:28 +0900

    Add lib.core.d.ts

--- lib.typhen.d.ts
+++ lib.typhen.d.ts
@@ -1,5 +1,8 @@
 /* tslint:disable:interface-name class-name */
 
+/// <reference no-default-lib="true"/>
+/// <reference path="./lib.core.d.ts" />
+
 interface TyphenPrimitiveType {}
 
 interface integer extends TyphenPrimitiveType {}
