0df794d58518cad4d42fc0829c1ef5eb3164c133
Shogo Iwano <shiwano@gmail.com>
2016-10-09 11:53:54 +0900

    Fix helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,17 +1,15 @@
 import * as _ from 'lodash';
 import * as inflection from 'inflection';
 
-function applyHelperToStringWithSeparator(str: any, helper: (str: string) => string): string {
-  if (typeof str === 'string') {
-    let separators = _.uniq(str.match(/[^a-z_0-9]+/gi));
+function applyHelperToStringWithSeparator(str: string, helper: (str: string) => string): string {
+  str = str.toString();
+  let separators = _.uniq(str.match(/[^a-z_0-9]+/gi));
 
-    if (separators.length === 1) {
-      return str.split(separators[0]).map(s => helper(s)).join(separators[0]);
-    } else {
-      return helper(str);
-    }
+  if (separators.length === 1) {
+    return str.split(separators[0]).map(s => helper(s)).join(separators[0]);
+  } else {
+    return helper(str);
   }
-  return str.toString();
 }
 
 export function underscore(str: string): string {
